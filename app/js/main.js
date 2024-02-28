763148d3e5a4edfe39a6d3d331d7a089ce80d848
Tarasdeveloper <tomahavki1@gmail.com>
2024-02-28 17:00:25 +0200

    favicon fix

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const header = document.querySelector('.header');
 
   window.onscroll = () => {
-    if (window.pageYOffset > 100) {
+    if (window.scrollY > 100) {
       header.classList.add('sticky');
     } else {
       header.classList.remove('sticky');
